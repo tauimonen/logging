2e7e6070977cf3c209aca20603b866eb1f1bcf6c
vgtoui <toni.uimonen@tuni.fi>
2023-02-02 13:37:53 +0200

    Create logging

--- Services/OrderService.cs
+++ Services/OrderService.cs
@@ -30,21 +30,34 @@ namespace LoggingAssignment.Services
             if(string.IsNullOrEmpty(order.ReferenceNumber))
             {
                 order.ReferenceNumber = ReferenceGenerator.GenerateReferenceNumber();
+                _log.Info($"Generated reference number for order {order.ReferenceNumber}.");
             }
 
+            _log.Debug($"Loading customer {order.CustomerId} from database.");
             var customer = _database.GetCustomer(order.CustomerId);
             if(customer.IsBusiness && !string.IsNullOrEmpty(customer.Email))
             {
                 try
                 {
+                    _log.Debug($"Sending email for customer {order.CustomerId}. Customer is a business customer and has email address set.");
                     MailService.SendEmail("New Order", $"Your order is successfully received.", customer.Email);
-                } catch (Exception ex) { }
+                } catch (Exception ex) { 
+                    _log.Error($"Failed to send email for {order.CustomerId}.", ex);
+                }
             }
+
+            if(!customer.IsBusiness) {
+                _log.Debug($"Customer {order.CustomerId} is not business customer. Skip the email sending.");
+            }
+
 
             try
             {
                 var result = _database.SaveOrder(order);
-            } catch(Exception ex) { }            
+                _log.Debug($"The order {order.ReferenceNumber} saved succesfully.");
+            } catch(Exception ex) { 
+                _log.Debug($"Failed to save the order {order.ReferenceNumber}.");
+            }            
         }
     }
 }
